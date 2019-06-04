0458d30b4044fa8da839d9768b354ca06926bd84
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-06-03 19:24:33 -0700

    Add some unit tests for `fastobo.load` error management

---
+++ tests/test_fastobo.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+import os
+import unittest
+
+import fastobo
+
+MS = os.path.realpath(os.path.join(__file__, "..", "data", "ms.obo"))
+
+
+class TestLoad(unittest.TestCase):
+
+    @unittest.expectedFailure
+    def test_file_not_found(self):
+        self.assertRaises(FileNotFoundError, fastobo.load, "abcdef")
+
+    def test_type_error(self):
+        self.assertRaises(TypeError, fastobo.load, 1)
+        self.assertRaises(TypeError, fastobo.load, [])
+
+        with open(MS) as f:
+            self.assertRaises(TypeError, fastobo.load, f)
+
+    def test_error_propagation(self):
+
+        def read(x):
+            if x == 0:
+                return b''
+            raise RuntimeError(x)
+
+        with open(MS, 'rb') as f:
+            f.read = read
+            self.assertRaises(RuntimeError, fastobo.load, f)

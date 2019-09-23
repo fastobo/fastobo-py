889046386b86c50217f41533a39f1bc6bfc82316
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-09-23 13:47:16 +0200

    Generate `pyproject.toml` on the fly when building a `sdist`

--- setup.py
+++ setup.py
@@ -4,6 +4,22 @@ import os
 
 import setuptools
 import setuptools_rust as rust
+from setuptools.command.sdist import sdist as _sdist
+
+
+class sdist(_sdist):
+
+    def run(self):
+        # build `pyproject.toml` from `setup.cfg`
+        c = configparser.ConfigParser()
+        c.add_section("build-system")
+        c.set("build-system", "requires", str(self.distribution.setup_requires))
+        with open("pyproject.toml", "w") as pyproject:
+            c.write(pyproject)
+
+        # run the rest of the packaging
+        _sdist.run(self)
+
 
 setuptools.setup(
     setup_requires=["setuptools", "setuptools_rust"],
@@ -11,6 +27,7 @@ setuptools.setup(
         "fastobo",
         path="Cargo.toml",
         binding=rust.Binding.PyO3,
-        strip=rust.Strip.Debug
+        strip=rust.Strip.Debug,
+        features=["extension-module"],
     )],
 )

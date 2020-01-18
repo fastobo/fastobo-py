445c5d03ffd261f48997a0742f19dbea642880c8
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-01-18 21:59:44 +0100

    Build on all supported versions only on tagged commits

--- ci/travis/osx/after_success.sh
+++ ci/travis/osx/after_success.sh
@@ -13,7 +13,7 @@ fi
 # --- Patch version number ---------------------------------------------------
 
 if [ -z "$TRAVIS_TAG" ]; then
-  VERSION=$($PYTHON setup.py --version)-dev$(git rev-list --count --all)
+  VERSION=$(python setup.py --version)-dev$(git rev-list --count --all)
   sed -i'.BAK' -e "s/version = $($PYTHON setup.py --version)/version = $VERSION/g" setup.cfg
 fi
 

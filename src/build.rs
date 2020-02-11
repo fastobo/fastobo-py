31ae91324fbdab41224d8f5f4d4512d7b38f4806
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-02-11 16:01:42 +0100

    Bump `built` dependency to `v0.4.0` and fix build script

--- src/build.rs
+++ src/build.rs
@@ -1,10 +1,10 @@
 extern crate built;
 
 fn main() {
-    let src = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let src = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
     let dst = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).join("built.rs");
     let mut opts = built::Options::default();
     opts.set_dependencies(true).set_compiler(true).set_env(true);
-    built::write_built_file_with_opts(&opts, src, dst)
+    built::write_built_file_with_opts(&opts, &src, &dst)
         .expect("Failed to acquire build-time information");
 }

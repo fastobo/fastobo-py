87ab994a7b03e4e92a798e17ea0008b38aae2dd6
Martin Larralde <martin.larralde@embl.de>
2020-06-12 19:36:43 +0200

    Add more tests for threading values of `fastobo` functions

--- tests/test_fastobo.py
+++ tests/test_fastobo.py
@@ -6,7 +6,7 @@ import unittest
 import fastobo
 
 MS = os.path.realpath(os.path.join(__file__, "..", "data", "ms.obo"))
-
+MS_FRAMES = 2941
 
 class TestLoad(unittest.TestCase):
 
@@ -34,6 +34,33 @@ class TestLoad(unittest.TestCase):
     def test_syntax_error(self):
         self.assertRaises(SyntaxError, fastobo.loads, "hello there")
 
+    def test_threading_single(self):
+        doc = fastobo.load(MS, threads=1)
+        self.assertEqual(len(doc), MS_FRAMES)
+
+        with open(MS, 'rb') as f:
+            doc = fastobo.load(f, threads=1)
+            self.assertEqual(len(doc), MS_FRAMES)
+
+    def test_threading_explicit(self):
+        doc = fastobo.load(MS, threads=4)
+        self.assertEqual(len(doc), MS_FRAMES)
+
+        with open(MS, 'rb') as f:
+            doc = fastobo.load(f, threads=4)
+            self.assertEqual(len(doc), MS_FRAMES)
+
+    def test_threading_detect(self):
+        doc = fastobo.load(MS, threads=0)
+        self.assertEqual(len(doc), MS_FRAMES)
+
+        with open(MS, 'rb') as f:
+            doc = fastobo.load(f, threads=0)
+            self.assertEqual(len(doc), MS_FRAMES)
+
+    def test_threading_invalid(self):
+        self.assertRaises(ValueError, fastobo.load, MS, threads=-1)
+
 
 class TestIter(unittest.TestCase):
 
@@ -46,3 +73,52 @@ class TestIter(unittest.TestCase):
 
         with open(MS) as f:
             self.assertRaises(TypeError, fastobo.iter, f)
+
+    def test_threading_single(self):
+        frame_count = sum(1 for _ in fastobo.iter(MS, threads=1))
+        self.assertEqual(frame_count, MS_FRAMES)
+
+        with open(MS, 'rb') as f:
+            frame_count = sum(1 for _ in fastobo.iter(f, threads=1))
+            self.assertEqual(frame_count, MS_FRAMES)
+
+    def test_threading_explicit(self):
+        frame_count = sum(1 for _ in fastobo.iter(MS, threads=4))
+        self.assertEqual(frame_count, MS_FRAMES)
+
+        with open(MS, 'rb') as f:
+            frame_count = sum(1 for _ in fastobo.iter(f, threads=4))
+            self.assertEqual(frame_count, MS_FRAMES)
+
+    def test_threading_detect(self):
+        frame_count = sum(1 for _ in fastobo.iter(MS, threads=0))
+        self.assertEqual(frame_count, MS_FRAMES)
+
+        with open(MS, 'rb') as f:
+            frame_count = sum(1 for _ in fastobo.iter(f, threads=0))
+            self.assertEqual(frame_count, MS_FRAMES)
+
+    def test_threading_invalid(self):
+        self.assertRaises(ValueError, fastobo.iter, MS, threads=-1)
+
+class TestLoads(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        with open(MS, 'r') as f:
+            cls.text = f.read()
+
+    def test_threading_single(self):
+        doc = fastobo.loads(self.text, threads=1)
+        self.assertEqual(len(doc), MS_FRAMES)
+
+    def test_threading_explicit(self):
+        doc = fastobo.loads(self.text, threads=4)
+        self.assertEqual(len(doc), MS_FRAMES)
+
+    def test_threading_detect(self):
+        doc = fastobo.loads(self.text, threads=0)
+        self.assertEqual(len(doc), MS_FRAMES)
+
+    def test_threading_invalid(self):
+        self.assertRaises(ValueError, fastobo.loads, self.text, threads=-1)

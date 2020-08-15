c6cc541223c795d75c63cf4318f6932483981609
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-08-15 14:58:46 +0200

    Modify `setup.py` to download the Rust compiler if it is not installed locally

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@
 
 import configparser
 import os
+import shutil
+import subprocess
+import sys
+import urllib.request
+from distutils.errors import DistutilsPlatformError
+from distutils.log import INFO
 
 import setuptools
 import setuptools_rust as rust
 from setuptools.command.sdist import sdist as _sdist
+from setuptools_rust.build import build_rust as _build_rust
 from setuptools_rust.utils import get_rust_version
 
 
@@ -24,20 +31,70 @@ class sdist(_sdist):
         _sdist.run(self)
 
 
-if "nightly" in get_rust_version().prerelease:
-    features = ["extension-module", "nightly"]
-else:
-    features = ["extension-module"]
+class build_rust(_build_rust):
+
+    def run(self):
+
+        try:
+            nightly = "nightly" in get_rust_version().prerelease
+        except DistutilsPlatformError:
+            if sys.platform in ("linux", "darwin"):
+                self.setup_temp_rustc_unix(toolchain="nightly", profile="minimal")
+                nightly = True
+            else:
+                nightly = False
+
+        if self.inplace:
+            self.extensions[0].strip = rust.Strip.No
+        if nightly:
+            self.extensions[0].features.append("nightly")
+
+        _build_rust.run(self)
+
+
+    def setup_temp_rustc_unix(self, toolchain, profile):
+
+        rustup_sh = os.path.join(self.build_temp, "rustup.sh")
+        os.environ["CARGO_HOME"] = os.path.join(self.build_temp, "cargo")
+        os.environ["RUSTUP_HOME"] = os.path.join(self.build_temp, "rustup")
+
+        self.mkpath(os.environ["CARGO_HOME"])
+        self.mkpath(os.environ["RUSTUP_HOME"])
+
+        self.announce("downloading rustup.sh install script", level=INFO)
+        with urllib.request.urlopen("https://sh.rustup.rs") as res:
+            with open(rustup_sh, "wb") as dst:
+                shutil.copyfileobj(res, dst)
+
+        self.announce(f"installing Rust compiler to {self.build_temp}", level=INFO)
+        subprocess.call([
+            "sh",
+            rustup_sh,
+            "-y",
+            "--default-toolchain",
+            toolchain,
+            "--profile",
+            profile,
+            "--no-modify-path"
+        ])
+
+        self.announce(f"updating $PATH variable to use local Rust compiler", level=INFO)
+        os.environ["PATH"] = ":".join([
+            os.path.abspath(os.path.join(os.environ["CARGO_HOME"], "bin")),
+            os.environ["PATH"]
+        ])
+
+
 
 
 setuptools.setup(
     setup_requires=["setuptools", "setuptools_rust"],
-    cmdclass=dict(sdist=sdist),
+    cmdclass=dict(sdist=sdist, build_rust=build_rust),
     rust_extensions=[rust.RustExtension(
         "fastobo",
         path="Cargo.toml",
         binding=rust.Binding.PyO3,
         strip=rust.Strip.Debug,
-        features=features,
+        features=["extension-module"],
     )],
 )
